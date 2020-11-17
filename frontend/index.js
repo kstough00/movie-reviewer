e00e812c674923b6397813e6818616e5702ae0bf
Your Username <you@example.com>
2020-11-17 16:18:57 -0700

    movie constructor class created

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,16 @@
-console.log("hello")
-
 function init() {
+    const containerDiv = document.getElementById("container");
 
+    function getMovies() {
+        fetch(`http://localhost:3000/movies`)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function (data) {
+
+        });
+    }
+    getMovies();
 }
 
-init()
+init();
---
+++ frontend/src/movie.js
@@ -0,0 +1,8 @@
+class Movie {
+    constructor(title, releaseYear, producer){
+        this.title = title
+        this.releaseYear = releaseYear
+        this.producer = producer
+        this.reviews = reviews
+    }
+}
