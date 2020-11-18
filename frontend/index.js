91d8d1f25ea15ff18779956e5ee43f91c2481e82
Your Username <you@example.com>
2020-11-18 15:41:29 -0700

    updated html a bit

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,29 @@ function init() {
 
     consoleForm.addEventListener('submit', (e) => {
         e.preventDefault()
+        let title = document.getElementById("title").value
+        let release_year = document.getElementById("release_year").value
+        let producer = document.getElementById("producer").value
+
+        let movie = {
+            title: title,
+            release_year: release_year,
+            producer: producer
+        }
+
+        fetch(`http://localhost:3000/movies` , {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(movie)
+        })
+        .then(resp => resp.json())
+        .then(movie => {
+            let m = new Movie(movie.id, movie.title, movie.release_year, movie.producer)
+            m.renderMovie();
+        })
         
         containerDiv.innerHTML += newMovie.renderIndexHTML();
     })
--- frontend/src/movie.js
+++ frontend/src/movie.js
@@ -1,12 +1,16 @@
 class Movie {
+
+    static all = []
+
     constructor(title, releaseYear, producer, reviews){
         this.title = title
         this.releaseYear = releaseYear
         this.producer = producer
         this.reviews = reviews
+        Movie.all.push
     }
     renderIndexHTML() {
-        return `<li>${this.title} - ${this.releaseYear} - ${this.producer} - ${this.reviews.length}<a href="http://localhost:3000/movies">Reviews</a></li>`;
+        return `<li>${this.title} - ${this.releaseYear} - ${this.producer} - ${this.reviews.length}<a href="http://localhost:3000/movies"> Reviews</a></li>`;
     }
 
     renderShowHTML() {
